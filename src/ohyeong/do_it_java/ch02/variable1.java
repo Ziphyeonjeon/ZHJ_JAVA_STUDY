aed6e7e063739449ad1c7acd03478c02d3316a29
ohyeong <oueong@naver.com>
2022-06-22 04:09:26 +0900

    ch2

---
+++ src/ohyeong/do_it_java/ch02/variable1.java
@@ -0,0 +1,9 @@
+package ohyeong.do_it_java.ch02;
+
+public class variable1 {
+    public static void main(String[] args) {
+        int level;
+        level=10;
+        System.out.println(level);
+    }
+}
